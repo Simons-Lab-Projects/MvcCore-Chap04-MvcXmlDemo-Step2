c127c31a975af2b869c15e7dba4fa563671b6a2a
Simon Woodworth <s.woodworth@ucc.ie>
2020-09-07 14:21:46 +0100

    Add project files.

---
+++ MvcXmlDemo/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcXmlDemo.Models;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace MvcXmlDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+         // POST: /Home/Index
+         [HttpPost]
+         public IActionResult Index(Category cat)
+         {
+            string msg;
+            try
+            {
+               XmlSerializer ser = new XmlSerializer(typeof(Category));
+               FileStream s = new FileStream("App_Data\\category.xml",
+                                             FileMode.Create);
+               ser.Serialize(s, cat);
+               s.Close();
+               msg = "Category has been saved";
+            }
+            catch (Exception ex)
+            {
+               msg = ex.Message;
+            }
+            ViewBag.Message = msg;
+            return View("Save", cat);
+         }
+
+         // GET: /Home/Restore
+         public IActionResult Restore()
+         {
+            string msg;
+            Category cat = new Category();
+            try
+            {
+               XmlSerializer ser = new XmlSerializer(typeof(Category));
+               FileStream s = new FileStream("App_Data\\category.xml",
+                                             FileMode.Open);
+               cat = (Category)ser.Deserialize(s);
+               s.Close();
+               msg = "Category has been restored";
+            }
+            catch (Exception ex)
+            {
+               msg = ex.Message;
+            }
+            ViewBag.Message = msg;
+            return View("Restore", cat);
+         }
+   }
+}
